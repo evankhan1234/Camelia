79e3985019aeb7c41e70a7a269115e388a73d24a
evankhan0123 <evankhan1234@gmail.com>
2020-02-20 16:02:19 +0600

    ok

---
+++ app/src/main/java/xact/idea/camelia/NetworkModel/HouseholdUploadModel.java
@@ -0,0 +1,49 @@
+package xact.idea.camelia.NetworkModel;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class HouseholdUploadModel {
+    @SerializedName("user_credential")
+    public String user_credential;
+    @SerializedName("data")
+    public ArrayList<Data> data;
+
+    public class Data {
+        @SerializedName("household_uniqe_id")
+        public String household_uniqe_id;
+        @SerializedName("division_id")
+        public String division_id;
+        @SerializedName("district_id")
+        public String district_id;
+        @SerializedName("upazila_id")
+        public String upazila_id;
+        @SerializedName("union_id")
+        public String union_id;
+        @SerializedName("ward_id")
+        public String ward_id;
+        @SerializedName("block_id")
+        public String block_id;
+        @SerializedName("village")
+        public String village;
+        @SerializedName("hh_number")
+        public String hh_number;
+        @SerializedName("sub_hh_number")
+        public String sub_hh_number;
+        @SerializedName("family_member")
+        public String family_member;
+        @SerializedName("income_per_month")
+        public String income_per_month;
+        @SerializedName("status")
+        public String status;
+        @SerializedName("created_at")
+        public String created_at;
+        @SerializedName("created_by")
+        public String created_by;
+        @SerializedName("updated_at")
+        public String updated_at;
+        @SerializedName("update_no")
+        public String update_no;
+    }
+}
---
+++ app/src/main/java/xact/idea/camelia/NetworkModel/MemberUploadModel.java
@@ -0,0 +1,67 @@
+package xact.idea.camelia.NetworkModel;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class MemberUploadModel {
+    @SerializedName("user_credential")
+    public String user_credential;
+    @SerializedName("data")
+    public ArrayList<Data> data;
+    public class Data {
+        @SerializedName("id")
+        public int id;
+        @SerializedName("household_uniqe_id")
+        public String household_uniqe_id;
+        @SerializedName("member_id")
+        public String member_id;
+        @SerializedName("unique_code")
+        public String unique_code;
+        @SerializedName("name")
+        public String name;
+        @SerializedName("mobile_number")
+        public String mobile_number;
+        @SerializedName("head_of_house")
+        public String natiohead_of_housenal_id;
+        @SerializedName("birth_date")
+        public String birth_date;
+        @SerializedName("date_of_data_collection")
+        public String date_of_data_collection;
+        @SerializedName("sex")
+        public String sex;
+        @SerializedName("religion")
+        public String religion;
+        @SerializedName("education")
+        public String education;
+        @SerializedName("marital_status")
+        public String marital_status;
+        @SerializedName("occupation")
+        public String occupation;
+        @SerializedName("blood_group")
+        public String blood_group;
+        @SerializedName("living_status")
+        public String living_status;
+        @SerializedName("death_date")
+        public String death_date;
+        @SerializedName("referred_to")
+        public String referred_to;
+        @SerializedName("refer_to_id")
+        public String refer_to_id;
+        @SerializedName("visit_date")
+        public String visit_date;
+        @SerializedName("note")
+        public String note;
+        @SerializedName("status")
+        public String status;
+        @SerializedName("created_at")
+        public String created_at;
+        @SerializedName("created_by")
+        public String created_by;
+        @SerializedName("updated_at")
+        public String updated_at;
+        @SerializedName("updated_by")
+        public String updated_by;
+
+    }
+}
