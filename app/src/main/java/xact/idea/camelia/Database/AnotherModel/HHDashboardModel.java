1b3db28a05595caf73a32f5e363356ab9756c767
evankhan0123 <evankhan1234@gmail.com>
2020-03-05 16:29:05 +0600

    done

---
+++ app/src/main/java/xact/idea/camelia/Database/AnotherModel/HHDashboardModel.java
@@ -0,0 +1,30 @@
+package xact.idea.camelia.Database.AnotherModel;
+
+import com.google.gson.annotations.SerializedName;
+
+public class HHDashboardModel {
+    @SerializedName("dibatis")
+    public int  dibatis;
+    @SerializedName("hypertension")
+    public int  hypertension;
+    @SerializedName("heartdisease")
+    public int  heartdisease;
+    @SerializedName("stroke")
+    public int  stroke;
+    @SerializedName("Lung")
+    public int  Lung;
+    @SerializedName("Ashma")
+    public int  Ashma;
+    @SerializedName("Kidney")
+    public int  Kidney;
+    @SerializedName("Cancer")
+    public int  Cancer;
+    @SerializedName("Mental")
+    public int  Mental;
+    @SerializedName("Male")
+    public int  Male;
+    @SerializedName("female")
+    public int  female;
+    @SerializedName("total_member")
+    public int  total_member;
+}
